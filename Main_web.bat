8f17e1222e31d7bc6e05aba98d69bc03eb936665
Azamat Mullagaliev <azamatmullagaliev@gmail.com>
2022-09-28 22:58:40 +0300

    deleted all unnecessary libraries

--- Main_web.bat
+++ Main_web.bat
@@ -1,4 +1,3 @@
-cd /users/azama/documents/wind_machine
 start chrome http://localhost:8080/
-node main_web2.js
+node main.js
 pause
--- main.js
+++ main.js
@@ -1,9 +1,25 @@
-const {Board, Servo} = require("johnny-five");
-const board = new Board({
-    port: "COM4"
-});
+var express = require('express');
+const { text } = require("express");
+app = express();
+server = require('http').createServer(app);
+io = require('socket.io')(server);
 
-board.on("ready",()=>{
-    const servo = new Servo(3);
-    servo.to(40);
+var SerialPort = require('serialport').SerialPort;
+var serialPort = new SerialPort({path: "/COM4", baudRate: 9600 });
+ 
+server.listen(8080);
+app.use(express.static('public'));
+ 
+var brightness;
+
+io.sockets.on('connection', function (socket) {
+        socket.on('led', function (data) {
+                brightness = data.value;
+                
+                serialPort.write("p"+brightness);
+                io.sockets.emit('led', {value: brightness});   
+                });
+        
+        socket.emit('led', {value: brightness});
 });
+console.log("Web Server Started go to 'http://localhost:8080' in your Browser.");
